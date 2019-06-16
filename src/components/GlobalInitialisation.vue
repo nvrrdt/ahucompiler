e0dfee82cb6c923eb122625c9891482bfcde1cd0
nvrrdt <nicoverrijdt@gmail.com>
2019-06-16 09:48:32 +0200

    The first adaptation to introduce the span's instead of inputs

--- src/components/GlobalInitialisation.vue
+++ src/components/GlobalInitialisation.vue
@@ -3,27 +3,27 @@
     <h1>ahucompiler</h1>
     <hr>
     <div class="row">
-      <div class="col-4">
-        <b-form-input id="keyProjectNumber" v-model="keyProjectNumber" type="text" readonly></b-form-input>
+      <div class="col-3">
+        <span>{{ keyProjectNumber }}</span>
       </div>
-      <div class="col-8">
-        <b-form-input id="valProjectNumber" v-model="valProjectNumber" type="text" autofocus tabindex="1"></b-form-input>
-      </div>
-    </div>
-    <div class="row">
-      <div class="col-4">
-        <b-form-input id="keyClient" v-model="keyClient" type="text" readonly></b-form-input>
-      </div>
-      <div class="col-8">
-        <b-form-input id="valClient" v-model="valClient" type="text" tabindex="2"></b-form-input>
+      <div class="col-9">
+        <span id="valProjectNumber" contenteditable="true" tabindex="1" @keyup="updateInputProjectNumber" ref="autofocus"></span>
       </div>
     </div>
     <div class="row">
-      <div class="col-4">
-        <b-form-input id="keyProjectName" v-model="keyProjectName" type="text" readonly></b-form-input>
+      <div class="col-3">
+        <span>{{ keyClient }}</span>
       </div>
-      <div class="col-8">
-        <b-form-input id="valProjectName" v-model="valProjectName" type="text" tabindex="3" @keydown.enter="$parent.$emit('onAddFormElement', 'UnitInitialisation')"></b-form-input>
+      <div class="col-9">
+        <span id="valClient" contenteditable="true" tabindex="2" @keyup="updateInputClient"></span>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-3">
+        <span>{{ keyProjectName }}</span>
+      </div>
+      <div class="col-9">
+        <span id="valProjectName" contenteditable="true" tabindex="3" @keyup="updateInputProjectName"></span>
       </div>
     </div>
     <hr>
@@ -44,9 +44,21 @@ export default {
     }
   },
   methods: {
+    updateInputProjectNumber: function () {
+      this.valProjectNumber = document.getElementById("valProjectNumber").innerText
+      console.log(this.valProjectNumber)
+    },
+    updateInputClient: function () {
+      this.valClient = document.getElementById("valClient").innerText
+      console.log(this.valClient)
+    },
+    updateInputProjectName: function () {
+      this.valProjectName = document.getElementById("valProjectName").innerText
+      console.log(this.valProjectName)
+    }
   },
   mounted: function () {
-    var inputs = document.querySelectorAll("input,select")
+    var inputs = document.querySelectorAll("span")
     for (var i = 0 ; i < inputs.length; i++) {
       inputs[i].addEventListener("keypress", function(e){
           if (e.which == 13) {
@@ -59,6 +71,9 @@ export default {
           }
       })
     }
+
+    // autofocus
+    this.$refs.autofocus.focus()
   }
 }
 </script>
